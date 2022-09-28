4690c7772dbf4bb2d3633e41f0049265aa48c945
Hope.Phenom <hope.phenom@foxmail.com>
2022-09-28 17:39:36 +0800

    [docs] 1. Add xmind design of Invoker

--- Invoker/InvokerConfigItem.cs
+++ Invoker/InvokerConfigItem.cs
@@ -6,16 +6,26 @@
     public class InvokerConfigItem
     {
         /// <summary>
-        /// Path of execute file.
-        /// </summary>
-        public string FilePath { get; set; } = "";
-        /// <summary>
-        /// Work folder of execute file.
-        /// </summary>
-        public string WorkFolder { get; set; } = "";
-        /// <summary>
         /// The startup sequence of the program.
         /// </summary>
         public int StartupIndex { get; set; } = 0;
+        /// <summary>
+        /// Path of file.
+        /// </summary>
+        public string FilePath { get; set; } = "";
+        /// <summary>
+        /// Work folder.
+        /// </summary>
+        public string WorkFolder { get; set; } = "";
+        /// <summary>
+        /// Type of file.
+        /// </summary>
+        public InvokeType Type { get; set; } = InvokeType.EXECUTABLE_FILE;
+    }
+
+    public enum InvokeType
+    { 
+        EXECUTABLE_FILE = 0,
+        SHELL
     }
 }
